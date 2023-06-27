7bc8627eee2459035e4d28c9ef49f18abafa3013
Daniel Springer <19246409+DaniSpringer@users.noreply.github.com>
2023-06-26 23:25:50 -0400

    docs

--- AppDelegate.swift
+++ AppDelegate.swift
@@ -21,10 +21,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     // TODO: todos
     // test kid-adult-boomer
 
-    // make Bonus level for all multiples and user has to say what it's a multiple of
-    // or just for unlocked multiples
-    // maybe let user choose which multiples to include in Bonus level
-
 
     // MARK: Life Cycle
 
